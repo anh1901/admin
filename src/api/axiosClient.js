ca3caf9775fb86044678f5f2473050f3e86463cf
67224153+anh1901@users.noreply.github.com <67224153+anh1901@users.noreply.github.com>
2022-08-10 18:37:57 +0700

    Add editor manager

--- src/api/axiosClient.js
+++ src/api/axiosClient.js
@@ -4,6 +4,8 @@ const axiosClient = axios.create({
   // baseURL: "http://ec2-52-221-205-151.ap-southeast-1.compute.amazonaws.com/api",
   //baseURL: "https://24hreportsystemapi.conveyor.cloud/api",
   baseURL: "https://domaintestapp.online/api",
+  //
+  
   headers: {
     "Content-Type": "application/json",
   },
